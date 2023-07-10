25e1f8dc9ea6b7e3cb8a3a5cdf3da000cfdff800
Brenda <brendasod98@gmail.com>
2023-07-10 19:36:56 -0300

    Validação com Bean Validation

--- api/src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ api/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,5 +1,26 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco(String logadouro, String bairro, String cep, String numero, String complemento, String cidade, String uf) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+		@NotBlank
+		String logadouro, 
+		
+		@NotBlank
+		String bairro, 
+		
+		@NotBlank
+		@Pattern(regexp = "\\d{8}")
+		String cep, 
+		
+		String numero, 		
+		String complemento,
+		
+		@NotBlank
+		String cidade, 
+		
+		@NotBlank
+		String uf) {
 
 }
--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,7 +1,30 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+		//notblanck verifica se não é nulo nem vazio
+		@NotBlank
+		String nome, 
+		
+		@NotBlank 
+		@Email
+		String email, 
+		
+		@NotBlank
+		@Pattern(regexp = "\\d{4,6}") //expressão regular
+		String crm, 
+		
+		@NotNull //porque não é string
+		Especialidade especialidade, 
+		
+		@NotNull
+		@Valid //validar também o objeto endereço
+		DadosEndereco endereco) {
 
 }
